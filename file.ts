c65c504d390b8cca3b7dc2ca86a4ed4d9056bd14
JamaliHassan <balochhassan939@gmail.com>
2024-03-30 10:39:48 -0700

    This is a simple command line Todo app

---
+++ file.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true
+while(condition){
+let task = await inquirer.prompt(
+
+[
+    {
+        name: "todo",
+        type: "input",
+        message: "What do you want to add in your Todos?"
+
+
+    },
+    {
+        name : 'addMore',
+        type: 'confirm',
+        message: 'Do you want to add More in your Todos',
+        default: 'false'
+
+
+
+
+    }
+]
+);
+todos.push(task.todo);
+condition = task.addMore
+console.log(todos)}
